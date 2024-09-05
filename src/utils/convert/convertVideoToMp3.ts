8f950daf3a260decc5018597c7bc0c8e5894c2d9
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-09-05 01:25:38 -0300

    add function convertVideoToMp3

---
+++ src/utils/convert/convertVideoToMp3.ts
@@ -0,0 +1,20 @@
+const ffmpeg = require("fluent-ffmpeg");
+
+export function convertVideoToMp3(
+  videoPath: string,
+  audioPath: string
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    ffmpeg(videoPath)
+      .output(audioPath)
+      .on("end", () => {
+        console.log("converted");
+        resolve();
+      })
+      .on("error", (err: Error) => {
+        console.log(err);
+        reject(err);
+      })
+      .run();
+  });
+}
