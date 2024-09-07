67d79f2167b77466869590ac6a8749f9f75b2dc5
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-09-07 02:03:11 -0300

    feat: add function addSubtitleToVideo in controller

--- src/utils/controller.ts
+++ src/utils/controller.ts
@@ -4,31 +4,38 @@ import fs from "fs";
 import { convertVideoToMp3 } from "./convert/convertVideoToMp3";
 import { processVideoToStr } from "./processVideoToStr";
 
+import ffmpeg from "fluent-ffmpeg";
+import { addSubtitleToVideo } from "./Ffmpeg/addSubtitleToVideo";
 
-const ffmpeg = require("fluent-ffmpeg");
-
-export function controllerFfmpeg(videoPath: string, res: Response, audioFilePath: string, caption: string, videoFile: Express.Multer.File) {
+export async function controllerFfmpeg(
+  videoPath: string,
+  res: Response,
+  audioFilePath: string,
+  caption: string,
+  videoFile: Express.Multer.File
+) {
   ffmpeg.ffprobe(
     videoPath,
     async (err: Error | null, metadata: FfprobeData) => {
       const durationInSecond = metadata.format.duration;
+      const subtitlePath = `src/srt/${videoFile?.filename}.srt`;
+      const videoWithSubtitlePath = `src/output/${videoFile?.filename}.mp4`;
 
       if (durationInSecond !== undefined && durationInSecond > 60) {
         fs.unlinkSync(videoPath);
         res.status(400).send("Video must have a maximum duration of 1 min");
       }
 
-      await convertVideoToMp3(
-        videoPath,
-        `audios/${videoFile?.filename}.mp3`
-      );
+      await convertVideoToMp3(videoPath, `src/audios/${videoFile?.filename}.mp3`);
 
       await processVideoToStr(
         process.env.API_KEY!,
         audioFilePath,
         caption,
-        `strFile/${videoFile?.filename}.str`
-      )
+        subtitlePath
+      );
+
+      await addSubtitleToVideo(videoPath, subtitlePath, videoWithSubtitlePath, audioFilePath);
     }
   );
 }
