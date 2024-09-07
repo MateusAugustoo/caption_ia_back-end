e7b90c160b1b14d883f59b72ce58cbfedfa307f2
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-09-07 02:00:26 -0300

    feat: generated arq .srt

--- src/utils/convert/covertToStr.ts
+++ src/utils/convert/covertToStr.ts
@@ -1,24 +1,27 @@
-export function convertToStr(transcription: any): any {
-  
+export function convertToSrt(transcription: any[]): string {
+  let str = "";
+
+  transcription.forEach((item: any, index: number) => {
+    const timeStart = formatTimeForSrt(item.start);
+    const timeEnd = formatTimeForSrt(item.end);
+
+    str += `${index + 1}\n${timeStart} --> ${timeEnd}\n${item.text}\n\n`;
+  });
+
+  return str;
 }
 
+function formatTimeForSrt(time: number) {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time - hours * 3600) / 60);
+  const seconds = Math.floor(time - hours * 3600 - minutes * 60);
+  const milliseconds = Math.floor(
+    (time - hours * 3600 - minutes * 60 - seconds) * 1000
+  );
 
-//espectativa 
-/*
-1
-00:00:00,000 --> 00:00:01,000
-Hello
-
-
-2
-00:00:01,000 --> 00:00:02,000
-World
-
-
-3
-00:00:02,000 --> 00:00:03,000
-!
-*/
-
-//realidade:
-// error
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}.${milliseconds
+    .toString()
+    .padStart(3, "0")}`;
+}
